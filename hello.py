d85c8928992226d36efcd23882708de7392f96d6
chamodi220 <chamodid220@gmail.com>
2024-12-26 22:32:18 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+
+# Ask the user for their favorite color
+color = input("What is your favorite color? ")
+
+# Display a message with the input
+print(f"Hello, {name}! It's cool that your favorite color is {color}.")
